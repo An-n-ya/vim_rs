93af8f76c6253259889c3d000033b0e166a726ea
Annya <annya.host@gmail.com>
2024-05-07 07:15:03 +0800

    covert filename to extension name in highlighter

--- src/highlight.rs
+++ src/highlight.rs
@@ -8,23 +8,24 @@ use syntect::{
 pub struct HighLighter {
     ps: SyntaxSet,
     ts: ThemeSet,
-    name: String,
+    extension: String,
 }
 
 impl HighLighter {
     pub fn new(name: &str) -> Self {
         let ps = SyntaxSet::load_defaults_newlines();
         let ts = ThemeSet::load_defaults();
-        // TODO: name to extension
+        let split: Vec<&str> = name.split(".").collect();
+        assert!(split.len() > 1);
         Self {
             ps,
-            name: name.to_string(),
+            extension: split.last().unwrap().to_string(),
             ts,
         }
     }
 
     pub fn highlight_line(&self, line: &str) -> String {
-        let syntax = self.ps.find_syntax_by_extension("rs").unwrap();
+        let syntax = self.ps.find_syntax_by_extension(&self.extension).unwrap();
         let theme = self.ts.themes["base16-ocean.dark"].clone();
         let mut h = HighlightLines::new(syntax, &theme);
         let ranges = h.highlight_line(line, &self.ps).unwrap();
